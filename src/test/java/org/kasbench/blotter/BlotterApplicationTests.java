aee7161ddb9b5718aa615119d493669a0e1bbbd8
Noah Krieger <noahkrieger@gmail.com>
2025-04-29 08:50:46 -0400

    initial setup.  work in progress.

---
+++ src/test/java/org/kasbench/blotter/BlotterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kasbench.blotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
